565950795a9849b9665e6df85158813a8f2cc7c6
Aerojace <114887413+Aerojace@users.noreply.github.com>
2022-11-29 00:32:05 -0700

    Add files via upload
    web scraping worldometer V1

--- ScrapeWebsite.py
+++ ScrapeWebsite.py
@@ -1,30 +1,44 @@
+import requests
+from bs4 import BeautifulSoup as soup
 import WriteJson
 
-def ScrapeAll(countrylist,websitelist):
-    for i in range(len(countrylist)):
-        for j in range(len(websitelist)):
-            scrape_country(countrylist[i],websitelist[j])
-    
-    return 1
 
-def scrape_country(country_name,website_name):
-    if website_name == "worldometer":
-        print("Getting data from Worldometer")
-        dailyD = 10
-        cumulativeD = 1
-        norm_dailyD = 1
-        norm_comulativeD = 1
-        WriteJson.data2json(website_name,country_name,dailyD,cumulativeD,norm_dailyD,norm_comulativeD)
-        return 1
-    elif website_name == "nytimes":
-        
-        print("Getting data from NYTimes")
-        dailyD = 1
-        cumulativeD = 1
-        norm_dailyD = 1
-        norm_comulativeD = 1
-        WriteJson.data2json(website_name,country_name,dailyD,cumulativeD,norm_dailyD,norm_comulativeD)
-        return 1
-    else:
-        print("Unknow Website")
-        return 0
+def scrape_country(Country,url):
+    #requesting to access url
+    url_req = requests.get(url, allow_redirects = True)
+
+    #will check to see if request was successful
+    if url_req.status_code ==  200:
+        print("Request was successful")
+
+    #using beautifulsoup to parse data
+    url_soup = soup(url_req.text,"html.parser")
+    #finds the data table with all country info
+    data_table = url_soup.find("table", id = "main_table_countries_today").find("tbody")
+    #splitting up table into rows with each country
+    country_rows = data_table.find_all("tr", style = "")
+
+
+    country_wanted = []
+    #loops into each row and seperates the data from each column and then appends the data
+    #if the first column matches with the specified country of interest 
+    #also deletes any unwanted columsn 
+    for row in country_rows:
+        country_columns = row.find_all("td")
+        country_info = [column.text.strip() for column in country_columns]
+        #deletes first column
+        del country_info[0]
+        #deletes total cases and new cases
+        del country_info[1:3]
+        #deletes other unwanted data
+        del country_info[3:8]
+        del country_info[4:14]
+        if country_info[0] == Country:
+            country_wanted.append(country_info)
+            print(country_wanted)
+            return country_wanted
+            
+# Url we want to scrape
+url = "https://www.worldometers.info/coronavirus/"
+Country = "France"
+scrape_country(Country,url)
