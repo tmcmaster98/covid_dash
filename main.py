0810f7592ba6c4a71c014c5d88a045bf5112128f
tmcmaster98 <118394944+tmcmaster98@users.noreply.github.com>
2022-11-30 16:52:24 -0700

    Added Scrape NYT

--- main.py
+++ main.py
@@ -4,26 +4,11 @@ import bs4 as BeautifulSoup
 import urllib.request
 
 # This will be the main function to run everything.
-'''
-countries = ['USA','India','France','Germany','Brazil','S. Korea','Italy','UK','Japan','Russia']
+
+countries = ['United States','India','France','Germany','Brazil','South Korea','Italy','U.K.','Japan','Russia']
 websites = ["worldometer","nytimes"]
 
 ScrapeWebsite.ScrapeAll(countries,websites)
 
 dataAll = WriteJson.readJson()
 
-'''
-with urllib.request.urlopen("https://www.nytimes.com/interactive/2021/us/covid-cases.html") as fp:
-    soup = BeautifulSoup.BeautifulSoup(fp,'html5lib')
-
-print(soup.title)
-print('Reported cases, deaths' in str(soup.get_text))
-"""
-with open("Data/NYT_11-26-2022.html") as fp:
-    data = BeautifulSoup(fp,'html5lib')
-data = BeautifulSoup("<html>a web page</html>", 'html.parser')
-"""
-#data = BeautifulSoup(fp,'html5lib')
-
-#    data = BeautifulSoup(fp, "html.parser")
-#print(data)
