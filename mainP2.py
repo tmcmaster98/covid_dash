2ff2604c4c6ae04d8208e2912bae68dd74d2f37d
ZacharyHUN <zahunz@comcast.net>
2022-12-03 18:18:35 -0700

    ploting

--- mainP2.py
+++ mainP2.py
@@ -1,7 +1,7 @@
 import ScrapeWebsite
 import WriteJson
 from datetime import datetime
-from bokeh.layouts import layout
+from bokeh.layouts import layout, column, row
 from bokeh.models import Div, CustomJS, Select, DateRangeSlider, ColumnDataSource, Legend
 from bokeh.plotting import figure, show
 
@@ -13,7 +13,7 @@ def str2date(dates):
         output[i] = datetime(int(brokenlist[2]),int(brokenlist[1]),int(brokenlist[0]),0,0,0)
     return output
 
-def getdatalist(data,countrylist,website,dates,datetype):
+def getdatalist(data,countrylist,website,dates,datetype,dates2):
     output = dict()
     outputline = [0]*len(dates)
     for i in range(0,len(countrylist)):
@@ -21,6 +21,7 @@ def getdatalist(data,countrylist,website,dates,datetype):
             outputline[j] = WriteJson.getData(data,website,countrylist[i],dates[j],datetype)
         output[countrylist[i]] = outputline
         outputline = [0]*len(dates)
+    output['x'] = dates2
     return output
 # This will be the main function to run everything.
 
@@ -33,33 +34,66 @@ websites = ["worldometer","nytimes"]
 dataAll = WriteJson.readJson()
 dates_str = dataAll["Dates"]
 dates = str2date(dates_str)
-Wtd = getdatalist(dataAll,countries,websites[0],dates_str,"cumulative death")
-Wntd = getdatalist(dataAll,countries,websites[0],dates_str,"normalized comulative death")
-Ntd = getdatalist(dataAll,countries,websites[-1],dates_str,"cumulative death")
-Nntd = getdatalist(dataAll,countries,websites[-1],dates_str,"normalized comulative death")
+Wtd = getdatalist(dataAll,countries,websites[0],dates_str,"cumulative death",dates)
+Wntd = getdatalist(dataAll,countries,websites[0],dates_str,"normalized comulative death",dates)
+Ntd = getdatalist(dataAll,countries,websites[-1],dates_str,"cumulative death",dates)
+Nntd = getdatalist(dataAll,countries,websites[-1],dates_str,"normalized comulative death",dates)
 
 divtop = Div(text="<b>Welcome to your Covid Dashboard </b>")
 divtop2 = Div(text="Created by Thomas Mcmaster, Jason Hernandez and Zachary Hunzeker")
 breakline = Div(text = "_________________________________________________________________________________________________________________________________________________________________________________________________________________________",height = 50)
 sourcedata1 = Wtd
-sourcedata1['x'] = dates
 source1 = ColumnDataSource(data = sourcedata1)
 selectWebsite = Select(title="Website",value=websites[0],options=websites)
-p1 = figure(width = 800, height = 500,x_axis_type="datetime")
-
+selectoutput = Select(title = "Value of interest",value = "Total Deaths", options=["Total Deaths","Normalized Total Deaths"])
+p1 = figure(width = 800, height = 500,x_axis_type="datetime",title = 'Deaths Over time',y_axis_label = "Total Deaths",x_axis_label = "Time")
 for z in range(0,len(countries)):
     line = countries[z]
     p1.line('x',line,source=source1,legend_label = countries[z],color = colorslist[z])
 
 p1.add_layout(Legend(),'right')
 p1.legend.click_policy = "hide"
+websiteS = 0
+dataS = 0
 
+callbackData = CustomJS(args=dict(source=source1, selectWebsite=selectWebsite,selectoutput=selectoutput,yaxis= p1.yaxis[0],Wtd=Wtd,Wntd=Wntd,Ntd=Ntd,Nntd=Nntd),code="""
+                const outputW = selectoutput.value
+                const web = selectWebsite.value
+                if (web == "worldometer"){
+                    if (outputW == "Total Deaths"){
+                        source.data = Wtd
+                        yaxis.axis_label = "Total Deaths"
+                    }
+                    else{
+                        source.data = Wntd
+                        yaxis.axis_label = "Normalized Total Deaths"
+                        
+                    }    
+                }
+                else{
+                    if (outputW == "Total Deaths"){
+                        source.data = Ntd
+                        yaxis.axis_label = "Total Deaths"
+                    }
+                    else{
+                        source.data = Nntd
+                        yaxis.axis_label = "Normalized Total Deaths"
+                    }
+                }         
+                        """)
+
+selectWebsite.js_on_change('value',callbackData)
+selectoutput.js_on_change('value',callbackData)
 date_range_slider1 = DateRangeSlider(title= "Choose date range",value = (dates[0],dates[-1]),start = dates[0] ,end = dates[-1])
 date_range_slider1.js_link('value',p1.x_range,'start',attr_selector=0)
 date_range_slider1.js_link('value',p1.x_range,'end',attr_selector=1)
+layoutfigure1 = row(p1,column(selectoutput,selectWebsite,date_range_slider1))
+## This is where the rest of the code goes
 
 
 
-output = layout([[divtop],[divtop2],[breakline],[selectWebsite],[date_range_slider1],[p1]])
+
+
+output = layout([[divtop],[divtop2],[breakline],[layoutfigure1],[breakline]])
 
 show(output)
