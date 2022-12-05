ea3959d6f8a6fb01e464ff1f83c48e672e4f7999
ZacharyHUN <zahunz@comcast.net>
2022-12-05 14:41:30 -0700

    fixed some of the xrange issues for my plot

--- mainP2.py
+++ mainP2.py
@@ -2,7 +2,7 @@ import ScrapeWebsite
 import WriteJson
 from datetime import datetime
 from bokeh.layouts import layout, column, row
-from bokeh.models import Div, CustomJS, Select, DateRangeSlider, ColumnDataSource, Legend
+from bokeh.models import Div, CustomJS, Select, DateRangeSlider, ColumnDataSource, Legend, Range1d
 from bokeh.plotting import figure, show
 
 # Functions used below
@@ -46,7 +46,9 @@ sourcedata1 = Wtd
 source1 = ColumnDataSource(data = sourcedata1)
 selectWebsite = Select(title="Website",value=websites[0],options=websites)
 selectoutput = Select(title = "Value of interest",value = "Total Deaths", options=["Total Deaths","Normalized Total Deaths"])
+date_range_slider1 = DateRangeSlider(title= "Choose date range",value = (dates[0],dates[-1]),start = dates[0] ,end = dates[-1])
 p1 = figure(width = 1000, height = 500,x_axis_type="datetime",title = 'Deaths Over time',y_axis_label = "Total Deaths",x_axis_label = "Time")
+p1.x_range = Range1d(date_range_slider1.value[0],date_range_slider1.value[1])
 legend_it = []
 for z in range(0,len(countries)):
     line = countries[z]
@@ -55,12 +57,13 @@ for z in range(0,len(countries)):
 legend = Legend(items= legend_it)
 p1.add_layout(legend,'right')
 p1.legend.click_policy = "hide"
-websiteS = 0
-dataS = 0
+divnote1 = Div(text = "Click on legend name to hide/reappear that countries data.")
+divnote2 = Div(text = "With the choose date range you can't have it be a single day.")
 
-callbackData = CustomJS(args=dict(source=source1, selectWebsite=selectWebsite,selectoutput=selectoutput,yaxis= p1.yaxis[0],Wtd=Wtd,Wntd=Wntd,Ntd=Ntd,Nntd=Nntd),code="""
+callbackData = CustomJS(args=dict(source=source1, selectWebsite=selectWebsite,selectoutput=selectoutput,yaxis= p1.yaxis[0],xrange = p1.x_range,date_range_slider1=date_range_slider1,Wtd=Wtd,Wntd=Wntd,Ntd=Ntd,Nntd=Nntd),code="""
                 const outputW = selectoutput.value
                 const web = selectWebsite.value
+
                 if (web == "worldometer"){
                     if (outputW == "Total Deaths"){
                         source.data = Wtd
@@ -81,15 +84,15 @@ callbackData = CustomJS(args=dict(source=source1, selectWebsite=selectWebsite,se
                         source.data = Nntd
                         yaxis.axis_label = "Normalized Total Deaths"
                     }
-                }         
+                }
+                data_range_slider1.value[0] = date_range_slider1.start
+                data_range_slider1.value[1] = date_range_slider1.end         
                         """)
-
-selectWebsite.js_on_change('value',callbackData)
-selectoutput.js_on_change('value',callbackData)
-date_range_slider1 = DateRangeSlider(title= "Choose date range",value = (dates[0],dates[-1]),start = dates[0] ,end = dates[-1])
 date_range_slider1.js_link('value',p1.x_range,'start',attr_selector=0)
 date_range_slider1.js_link('value',p1.x_range,'end',attr_selector=1)
-layoutfigure1 = row(p1,column(selectoutput,selectWebsite,date_range_slider1))
+selectWebsite.js_on_change('value',callbackData)
+selectoutput.js_on_change('value',callbackData)
+layoutfigure1 = row(p1,column(selectoutput,selectWebsite,date_range_slider1,divnote1,divnote2))
 ## This is where the rest of the code goes
 
 
